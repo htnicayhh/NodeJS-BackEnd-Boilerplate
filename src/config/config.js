4e269caaeff21616ba3c9c2e6a2291d3b050e148
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-04 22:09:09 +0700

    add jwt_secret

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 export default {
     PORT: process.env.DB_PORT || 3306,
     MYSQL_URL: process.env.MYSQL_URL || 'mysql://root:Hyacinth130620@localhost:3306/todoapp',
-    JWT_SECRET: process.env.TOKEN_SECRET || 'htnica-yh'
+    JWT_SECRET: process.env.TOKEN_SECRET || 'htnica-yh',
+    JWT_REFRESH_TOKEN: process.env.REFRESH_TOKEN || 'htnica_yh6'
 }
