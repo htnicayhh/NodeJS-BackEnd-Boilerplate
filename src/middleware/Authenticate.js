ccf02248d6f5248968128376ffae6af0ffd52e9e
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-08 02:07:21 +0700

    fix authen

--- src/middleware/Authenticate.js
+++ src/middleware/Authenticate.js
@@ -3,18 +3,22 @@ import { ERRORS } from '../constant/Errors'
 
 export const jwtToken = async (req, res, next) => {
     const authorization = req.headers['authorization'] // 'Beaer [token]'
-    const token = authorization.split(' ')[1] // Get token
-    if (token) {
-        try {
-            const tokenValid = await JwtUtil.verifyToken(token, Config.JWT_SECRET)
-            req.tokenValid = tokenValid // Save into req for later processing...
-            next()
-        } catch (error) {
-            res.sendStatus(ERRORS.UNAUTHORIZE_ERROR)
+    
+    if (authorization && authorization.match(/^Beaer /g)) {
+        const token = authorization.split(' ')[1] // Get token
+        if (token) {
+            try {
+                const tokenValid = await JwtUtil.verifyToken(token)
+                req.tokenValid = tokenValid // Save into req for later processing...
+                next()
+            } catch (error) {
+                res.sendStatus(ERRORS.UNAUTHORIZE_ERROR)
+            }
         }
     } else {
         next(ERRORS.TOKEN_REQUIRED)
     }
+    
 }
 
 export const refreshToken = async (req, res, next) => {
