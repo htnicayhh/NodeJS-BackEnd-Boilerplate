158885ee31d42533a0bc3b899c2e4a8c13da5297
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-12 00:16:11 +0700

    create badRequest

--- src/constant/Errors.js
+++ src/constant/Errors.js
@@ -3,5 +3,6 @@ import createError from 'http-errors'
 export const ERRORS = {
     SIGNATURE_ERROR: createError(400, 'BAD_SIGNATURE'),
     UNAUTHORIZE_ERROR: createError(401, 'UNAUTHORIZE_ERROR'),
-    TOKEN_REQUIRED: createError(403, 'TOKEN_REQUIRED')
+    TOKEN_REQUIRED: createError(403, 'TOKEN_REQUIRED'),
+    TOKEN_NOT_ALLOW: createError(401, 'TOKEN_NOT_ALLOW')
 }
