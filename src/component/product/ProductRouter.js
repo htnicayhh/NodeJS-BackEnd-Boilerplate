4216df2c586fe141e3c619620573310769d94a85
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-13 02:05:11 +0700

    pagination allProduct

--- src/component/product/ProductRouter.js
+++ src/component/product/ProductRouter.js
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { errorHandler } from '../../middleware/ErrorHandler'
 import { jwtToken } from '../../middleware/Authenticate'
 import { isAdmin } from '../../middleware/Authorize'
+import { Pagination } from '../../middleware/Pagination'
 import {
     getAllProductController,
     getProductByIDController,
@@ -14,7 +15,7 @@ import {
 const path = '/product'
 const router = Router()
 
-router.get('/all', jwtToken, errorHandler(getAllProductController))
+router.get('/all', jwtToken, Pagination(1, 8), errorHandler(getAllProductController))
 router.get('/search', jwtToken, errorHandler(searchProductController))
 router.get('/:ID', jwtToken, errorHandler(getProductByIDController))
 router.post('/create', jwtToken, errorHandler(createProductController))
