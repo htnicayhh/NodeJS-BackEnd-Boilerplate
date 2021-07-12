3e77a8ed32e12074b0daf16efbbaa16c4eab19e8
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-12 16:42:23 +0700

    add login & regist

--- src/component/auth/AuthController.js
+++ src/component/auth/AuthController.js
@@ -4,31 +4,50 @@ import * as jwtUtil from '../../util/Jwt'
 import { response } from '../../util/ResponseCheck'
 import { TOKEN } from '../../constant/Token'
 import { ERRORS } from '../../constant/Errors'
-
-let admin = {
-    username: 'hynica-yh',
-    password: 'htnicayh_6',
-    email: 'domain@gmail.com',
-    authorize: 'admin'
-}
+import { hashSync, compareSync } from '../../util/Bcrypt' 
 
 export const login = async (req, res, next) => {
-    
+    const { username, password } = req.body
+    if (username && password) {
+        const customers = await authDAL.getUserByName(username)
+        if (customers) {
+            const pwIsValid = await compareSync(password, customers.password)
+            if (pwIsValid) {
+                let data = {
+                    userId: customers.id,
+                    userName: customers.username,
+                    authorize: customers.authorize
+                }
+                const tokenValid = await jwtUtil.createToken(data, { expiresIn: TOKEN.TOKEN_EXPIRED })
+                let token = {
+                    token: tokenValid,
+                    timeExpires: TOKEN.TOKEN_EXPIRED // ms
+                }
+                res.send(response(token))
+            } else {
+                next(ERRORS.INVALID_USERNAME_OR_PASSWORD_ERROR)
+            }
+        } else {
+            next(ERRORS.INVALID_USERNAME_OR_PASSWORD_ERROR)
+        }
+    } else {
+        next(ERRORS.INVALID_INPUT_PARAMS)
+    }
+
 }
 
 export const regist = async (req, res, next) => {
-
-}
-
-export const getUser = (req, res, next) => {
-    res.json({
-        status: 'Access Token',
-        data: admin
-    })
-}
-
-export const createToken = async (req, res, next) => {
-    const data = req.body
-    const result = await jwtUtil.createToken(data)
-    res.json(result)
+    const { username, password, email } = req.body
+    if (username && password) {
+        const customers = await authDAL.getUserByName(username)
+        if (customers) {
+            next('USER_ALREADY_EXISTS')
+        } else {
+            const pwHash = hashSync(password)
+            await authDAL.createAccount(username, password, email)
+            res.send(response(req.body))
+        }
+    } else {
+        next(ERRORS.INVALID_INPUT_PARAMS)
+    }
 }
