78761965fa049535bb1e39447a00b84993ff6fab
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-13 01:58:18 +0700

    d

--- src/middleware/errorHandler.js
+++
@@ -1,8 +0,0 @@
-export const errorHandler = f => async (req, res, next) => {
-    try {
-        const handler = await f(req, res, next)
-        return handler
-    } catch (error) {
-        next(error)
-    }
-}
