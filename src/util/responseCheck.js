4e9f5dbdca30b53dbfb94dd290ee35e228fdb180
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-13 02:00:41 +0700

    d

--- src/util/responseCheck.js
+++
@@ -1,8 +0,0 @@
-import { STATUS } from "../constant/Status.js"
-
-export const response = 
-    (DATA,
-    CODE = STATUS.NOT_ERROR.CODE,
-    MESSAGE = STATUS.NOT_ERROR.MESSAGE) => {
-        return { CODE, MESSAGE, DATA }
-    }
