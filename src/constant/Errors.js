6bf0d989c0f077154d9d833d7fcced2030452980
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-12 16:43:06 +0700

    create badRequest

--- src/constant/Errors.js
+++ src/constant/Errors.js
@@ -4,5 +4,7 @@ export const ERRORS = {
     SIGNATURE_ERROR: createError(400, 'BAD_SIGNATURE'),
     UNAUTHORIZE_ERROR: createError(401, 'UNAUTHORIZE_ERROR'),
     TOKEN_REQUIRED: createError(403, 'TOKEN_REQUIRED'),
-    TOKEN_NOT_ALLOW: createError(401, 'TOKEN_NOT_ALLOW')
+    TOKEN_NOT_ALLOW: createError(401, 'TOKEN_NOT_ALLOW'),
+    INVALID_USERNAME_OR_PASSWORD_ERROR: createError(400, 'Username or password incorrect'),
+    INVALID_INPUT_PARAMS: createError(400, 'Invalid input params')
 }
