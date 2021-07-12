9024beeab62b4a7f9b15a41f317c0f992f6265ec
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-13 02:00:17 +0700

    database util

---
+++ src/util/Database.js
@@ -0,0 +1,32 @@
+import mysql from 'mysql'
+import config from '../config/Config.js'
+
+// create database
+const connection = mysql.createPool(config.MYSQL_URL);
+
+export const query = async (sql, params) => {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, params, (error, results) => {
+            if (error) {
+                return reject(error);
+            }
+            return resolve(results);
+        });
+    });
+};
+
+export const queryOne = async (sql, params) => {
+    const results = await query(sql, params)
+    return results[0]
+}
+
+export const getConnection = async () => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+          if (err) {
+            return reject(err);
+          }
+          return resolve(connection);
+        });
+    });
+}
