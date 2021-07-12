05ce6e1f7334968b39f76f92c18361b7289f9999
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-13 01:57:52 +0700

    update status

---
+++ src/constant/Status.js
@@ -0,0 +1,18 @@
+export const STATUS = {
+    NOT_ERROR: {
+        CODE: 0,
+        MESSAGE: 'NOT_ERROR'
+    },
+    SERVER_ERROR: {
+        CODE: 1,
+        MESSAGE: 'SERVER_ERROR'
+    },
+    CLIENT_ERROR: {
+        CODE: 2,
+        MESSAGE: 'CLIENT_ERROR'
+    },
+    UKN_ERROR: {
+        CODE: -1,
+        MESSAGE: 'UKN_ERROR'
+    }
+ }
