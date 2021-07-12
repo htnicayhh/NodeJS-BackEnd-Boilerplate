e25d84ad3de0d5db15aca84b17e45d6eefee4bf8
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-12 23:10:06 +0700

    update Router

--- src/component/router.js
+++ src/component/router.js
@@ -1,11 +1,9 @@
-import ProductRouter from './product/getAllProduct/ProductRouter'
-import CrudRouter from './product/crudProduct/CrudRouter'
+import ProductRouter from './product/ProductRouter'
 import DefaultRouter from './default/DefaultRouter'
 import AuthRouter from './auth/AuthRouter'
 
 export default [
     DefaultRouter,
     ProductRouter,
-    CrudRouter,
     AuthRouter
 ]
