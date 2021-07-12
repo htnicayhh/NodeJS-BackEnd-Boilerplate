970379a12e9d5b24e99080afc00fab8fc1af0b9b
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-12 22:17:16 +0700

    delete

--- src/component/product/getAllProduct/ProductDAL.js
+++
@@ -1,7 +0,0 @@
-import { query } from '../../../util/Database'
-
-export const getAllProductDAL = async () => {
-    let sql = 'select * from products order by ID_Cate'
-    const res = await query(sql)
-    return res
-}
