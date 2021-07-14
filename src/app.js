8e35e6257cbb2f2746f1350d3c661f1c6d677459
Dao Ba Hieu <81920675+htnica-yh@users.noreply.github.com>
2021-07-14 23:59:22 +0700

    @Cors Middleware

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import morgan from 'morgan'
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import routers from './component/router'
-import corsMiddleware from './middleware/Cors'
+import { corsMiddleware } from './middleware/Cors.js'
 
 const PORT = 3000
 const app = express()
@@ -25,7 +25,7 @@ app.use(corsMiddleware)
 // initialize routers
 for (const router of routers) {
     app.use(router.path, router.router)
-  }
+}
 
 // App Listen
 app.listen(PORT, () => {
